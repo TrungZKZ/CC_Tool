7942d5841c946cd9995cc565892ad6ecc3a5dd73
TrungZKZ <82562265+TrungZKZ@users.noreply.github.com>
2024-01-03 18:04:42 +0700

    Create stdafx.cpp

---
+++ CC_Tool/stdafx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp: only standard include CC_Tool.pch
+// The containing source file becomes a precompiled header.
+// stdafx.obj contains precompiled type information.
+
+#include "stdafx.h"
+
+
